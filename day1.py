580f9ca5a0b751601ae72f9d66410125dda70d71
Doug Hammond <douglas@gohiring.com>
2021-12-01 09:50:44 +0100

    More compact, more python-ish

--- day1.py
+++ day1.py
@@ -4,18 +4,9 @@ from get_aoc import get_input_integers
 
 depths = get_input_integers(1)
 
-increments = 0
-for i in range(len(depths) - 1):
-    if depths[i] < depths[i+1]:
-        increments += 1
-
 print("Part 1:")
-print(increments)
-
-increments = 0
-for i in range(len(depths) - 3):
-    if sum(depths[i:i+3]) < sum(depths[i+1:i+4]):
-        increments += 1
+print(sum(a < b for (a, b) in zip(depths[:-1], depths[1:])))
 
 print("Part 2:")
-print(increments)
+windows = list(zip(depths[:-2], depths[1:-1], depths[2:]))
+print(sum(sum(a) < sum(b) for (a, b) in zip(windows[:-1], windows[1:])))
