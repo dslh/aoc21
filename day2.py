a5e5696395d6cfbd63014ba6d757e326bce36eea
Doug Hammond <douglas@gohiring.com>
2021-12-02 21:34:58 +0100

    Part 2

--- day2.py
+++ day2.py
@@ -9,7 +9,21 @@ def total_direction(instructions, direction):
     return sum(distance for (direct, distance) in instructions if direction == direct)
 
 forward = total_direction(instructions, 'forward')
-down = total_direction(instructions, 'down') - total_direction(instructions, 'up')
+depth = total_direction(instructions, 'down') - total_direction(instructions, 'up')
 
 print("Part 1:")
-print(forward * down)
+print(forward * depth)
+
+depth = 0
+dir = 0
+
+for instruction, amount in instructions:
+    if instruction == 'forward':
+        depth += amount * dir
+    elif instruction == 'down':
+        dir += amount
+    elif instruction == 'up':
+        dir -= amount
+
+print("Part 2:")
+print(forward * depth)
