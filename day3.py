95fc469b8aa2a1e46d003d055cc69ae2d7726fa7
Doug Hammond <douglas@gohiring.com>
2021-12-03 23:25:20 +0100

    Day 3 part 2

--- day3.py
+++ day3.py
@@ -6,11 +6,10 @@ lines = get_input_lines(3)
 WIDTH = range(len(lines[0]))
 numbers = [int(line, 2) for line in lines]
 
-counts = {i:0 for i in WIDTH}
-for number in numbers:
-    for i in WIDTH:
-        if (1 << i) & number:
-            counts[i] += 1
+def count_position(numbers, position):
+    return sum((number & (1 << position)) != 0 for number in numbers)
+
+counts = [count_position(numbers, i) for i in WIDTH]
 
 threshold = len(lines) / 2
 
@@ -25,3 +24,36 @@ for i in WIDTH:
 
 print("Part 1:")
 print(gamma * epsilon)
+
+def find_oxygen_rating(numbers):
+    for i in reversed(WIDTH):
+        count = count_position(numbers, i)
+        threshold = len(numbers) / 2
+
+        if count >= threshold:
+            numbers = [n for n in numbers if n & (1 << i)]
+        else:
+            numbers = [n for n in numbers if ~n & (1 << i)]
+
+        if len(numbers) == 1:
+            return numbers[0]
+
+oxygen_rating = find_oxygen_rating(numbers)
+
+def find_scrubber_rating(numbers):
+    for i in reversed(WIDTH):
+        count = count_position(numbers, i)
+        threshold = len(numbers) / 2
+
+        if count >= threshold:
+            numbers = [n for n in numbers if ~n & (1 << i)]
+        else:
+            numbers = [n for n in numbers if n & (1 << i)]
+
+        if len(numbers) == 1:
+            return numbers[0]
+
+scrubber_rating = find_scrubber_rating(numbers)
+
+print("Part 2:")
+print(oxygen_rating * scrubber_rating)
