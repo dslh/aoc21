b668cb6e1970440ac418b1079d4ff7364b6c2369
Doug Hammond <douglas@gohiring.com>
2021-12-07 08:44:25 +0100

    Day 7 part 2

--- day7.py
+++ day7.py
@@ -3,11 +3,24 @@
 from get_aoc import get_input_integers
 
 crabs = get_input_integers(7, ',')
+crab_range = range(min(crabs), max(crabs))
 
 def fuel_cost(crabs, pos):
     return sum(abs(crab - pos) for crab in crabs)
 
-best_pos = min(range(min(crabs), max(crabs)), key=lambda pos : fuel_cost(crabs, pos))
+best_pos = min(crab_range, key=lambda pos : fuel_cost(crabs, pos))
 
 print("Part 1:")
 print(fuel_cost(crabs, best_pos))
+
+def geometric_cost(crab, pos):
+    dist = abs(crab - pos)
+    return dist * (dist + 1) // 2
+
+def geometric_fuel_cost(crabs, pos):
+    return sum(geometric_cost(crab, pos) for crab in crabs)
+
+best_pos = min(crab_range, key=lambda pos: geometric_fuel_cost(crabs, pos))
+
+print("Part 2:")
+print(geometric_fuel_cost(crabs, best_pos))
