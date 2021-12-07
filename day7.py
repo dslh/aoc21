58daa061b03864ca2515ad6766029e6d78fbdcd1
Doug Hammond <d.lakehammond@gmail.com>
2021-12-07 14:17:00 +0100

    Use stats, dummy

--- day7.py
+++ day7.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
 
 from get_aoc import get_input_integers
+import statistics, math
 
 crabs = get_input_integers(7, ',')
-crab_range = range(min(crabs), max(crabs))
 
-def fuel_cost(crabs, pos):
-    return sum(abs(crab - pos) for crab in crabs)
-
-best_pos = min(crab_range, key=lambda pos : fuel_cost(crabs, pos))
+best_pos = int(statistics.median(crabs))
 
 print("Part 1:")
-print(fuel_cost(crabs, best_pos))
+print(sum(abs(crab - best_pos) for crab in crabs))
 
 def geometric_cost(crab, pos):
     dist = abs(crab - pos)
@@ -20,7 +17,8 @@ def geometric_cost(crab, pos):
 def geometric_fuel_cost(crabs, pos):
     return sum(geometric_cost(crab, pos) for crab in crabs)
 
-best_pos = min(crab_range, key=lambda pos: geometric_fuel_cost(crabs, pos))
+best_pos = statistics.mean(crabs)
+cost = min(geometric_fuel_cost(crabs, pos) for pos in [math.floor(best_pos), math.ceil(best_pos)])
 
 print("Part 2:")
-print(geometric_fuel_cost(crabs, best_pos))
+print(cost)
