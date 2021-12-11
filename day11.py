c354a53b669716ae37f4fcf9ddb5e1ecd69308ae
Doug Hammond <d.lakehammond@gmail.com>
2021-12-11 18:27:30 +0100

    Seemed like it needed to be animated

--- day11.py
+++ day11.py
@@ -2,6 +2,42 @@
 
 from get_aoc import get_input_lines
 
+import os
+from time import sleep
+from colorama import init, Fore, Back, Style
+init()
+
+COLOURS = [
+    Fore.CYAN  + Back.CYAN  + Style.BRIGHT,
+    Fore.WHITE + Back.BLACK + Style.DIM,
+    Fore.WHITE + Back.BLACK + Style.DIM,
+    Fore.WHITE + Back.BLACK + Style.DIM,
+    Fore.CYAN  + Back.BLACK + Style.DIM,
+    Fore.CYAN  + Back.BLACK + Style.DIM,
+    Fore.CYAN  + Back.BLACK + Style.DIM,
+    Fore.CYAN  + Back.BLACK + Style.NORMAL,
+    Fore.CYAN  + Back.BLACK + Style.NORMAL,
+    Fore.CYAN  + Back.BLACK + Style.BRIGHT
+]
+CHARS = ['o','◦','•','☣','⦾','⦿','❅','❆','☢','❄']
+
+# Display the grid, all pretty and stuff
+def display(grid):
+    os.system('clear')
+
+    for _ in range(5):
+        print()
+
+    for line in grid:
+        print(' ' * 10, end='')
+
+        for x in line:
+            print(COLOURS[x] + CHARS[x], end='')
+
+        print(Style.RESET_ALL)
+
+    sleep(0.05)
+
 grid = [[int(i) for i in line] for line in get_input_lines(11)]
 HEIGHT = len(grid)
 WIDTH = len(grid[0])
@@ -35,14 +71,17 @@ def step(grid):
             if adjacent not in flashed:
                 increment(grid, *adjacent, flashing)
 
+    display(grid)
     return len(flashed)
 
-print("Part 1:")
-print(sum(step(grid) for _ in range(100)))
+display(grid)
+part_1 = sum(step(grid) for _ in range(100))
 
-count = 101
+part_2 = 101
 while step(grid) != WIDTH * HEIGHT:
-    count += 1
+    part_2 += 1
 
+print("Part 1:")
+print(part_1)
 print("Part 2:")
-print(count)
+print(part_2)
