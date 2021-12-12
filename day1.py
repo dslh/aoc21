25fed8d2869b48b4331a55ad8effbc6b0f872fb6
Doug Hammond <d.lakehammond@gmail.com>
2021-12-12 01:02:56 +0100

    More professionaler

--- day1.py
+++ day1.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
 
-from get_aoc import get_input_integers
+from collections import deque
 
-depths = get_input_integers(1)
+def windowed(iterable, size=2):
+    q = deque([], size)
+    for item in iterable:
+        q.append(item)
+        if len(q) == size:
+            yield list(q)
 
-print("Part 1:")
-print(sum(a < b for (a, b) in zip(depths[:-1], depths[1:])))
+def count_increases(lst, window=1):
+    return sum(sum(a) < sum(b) for (a, b) in windowed(windowed(lst, window), 2))
 
-print("Part 2:")
-windows = list(zip(depths[:-2], depths[1:-1], depths[2:]))
-print(sum(sum(a) < sum(b) for (a, b) in zip(windows[:-1], windows[1:])))
+if __name__ == '__main__':
+    from get_aoc import get_input_integers
+
+    depths = get_input_integers(1)
+
+    print("Part 1:")
+    print(count_increases(depths))
+
+    print("Part 2:")
+    print(count_increases(depths, 3))
