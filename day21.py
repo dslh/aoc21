de051fc1a85fd0aefbfd2ffdd8c0d9645d5beac3
Doug Hammond <d.lakehammond@gmail.com>
2021-12-21 20:16:49 +0100

    Day 21 parts 1 and 2

--- day21.py
+++ day21.py
@@ -7,15 +7,18 @@ def deterministic_die():
         value %= 100
         value += 1
 
-def play(positions, die):
+def play_deterministic(positions):
     players = len(positions)
     scores = [0] * players
 
     player = 0
     rolls = 0
+    die = 0
 
     while max(scores) < 1000:
-        roll = next(die) + next(die) + next(die)
+        roll = die * 3 + 6
+        die += 3
+        die %= 100
         rolls += 3
 
         positions[player] = ((positions[player] - 1 + roll) % 10) + 1
@@ -25,9 +28,60 @@ def play(positions, die):
 
     return sum(score for score in scores if score < 1000) * rolls
 
+# Distribution when rolling three three-sided dice
+ROLL_DISTRIBUTION = {
+    (3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)
+}
+
+class Game:
+    def __init__(self, positions, scores, turn):
+        self.positions = positions
+        self.scores = scores
+        self.turn = turn
+
+    def play(self, roll):
+        positions = self.positions.copy()
+        scores = self.scores.copy()
+        positions[self.turn] = ((positions[self.turn] - 1 + roll) % 10) + 1
+        scores[self.turn] += positions[self.turn]
+
+        return Game(positions, scores, (self.turn + 1) % 2)
+
+    def won(self):
+        if self.scores[0] >= 21:
+            return Wins(1, 0)
+        elif self.scores[1] >= 21:
+            return Wins(0, 1)
+
+class Wins:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __add__(self, other):
+        return Wins(self.a + other.a, self.b + other.b)
+
+    def __mul__(self, scalar):
+        return Wins(self.a * scalar, self.b * scalar)
+
+    def tuple(self):
+        return (self.a, self.b)
+
+def count_wins(game):
+    won = game.won()
+    if won: return won
+
+    return sum((count_wins(game.play(roll)) * freq for roll, freq in ROLL_DISTRIBUTION), Wins(0, 0))
+
+def play_quantum(positions):
+    return count_wins(Game(positions, [0,0], 0)).tuple()
+
 if __name__ == '__main__':
     from get_aoc import get_input_lines
     positions = [int(line.split(':')[1]) for line in get_input_lines(21)]
 
     print('Part 1:')
-    print(play(positions, iter(deterministic_die())))
+    print(play_deterministic(positions.copy()))
+
+    print('Part 2:')
+    print(max(play_quantum(positions)))
--- tests/test_day21.py
+++ tests/test_day21.py
@@ -1,21 +1,10 @@
 import pytest
 import context
 
-from day21 import deterministic_die, play
-
-def test_deterministic_die():
-    die = iter(deterministic_die())
-    assert next(die) == 1
-    assert next(die) == 2
-    assert next(die) == 3
-    assert next(die) == 4
-
-    for i in range(5,100):
-        assert next(die) == i
-
-    assert next(die) == 100
-    assert next(die) == 1
-    assert next(die) == 2
+from day21 import play_deterministic, play_quantum
 
 def test_part_one():
-    assert play([4,8], iter(deterministic_die())) == 739785
+    assert play_deterministic([4,8]) == 739785
+
+def test_part_two():
+    assert play_quantum([4,8]) == (444356092776315, 341960390180808)
